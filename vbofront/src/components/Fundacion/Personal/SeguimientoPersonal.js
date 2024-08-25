d904e1d09235aa840f938d1196476384ba714222
tasajos <tasajos@gmail.com>
2024-08-25 16:08:52 -0400

    update_seguimientoPErsonal

--- vbofront/src/components/Fundacion/Personal/SeguimientoPersonal.js
+++ vbofront/src/components/Fundacion/Personal/SeguimientoPersonal.js
@@ -22,6 +22,8 @@ function SeguimientoPersonal() {
   const [showEstadoModal, setShowEstadoModal] = useState(false);
   const [showHistorial, setShowHistorial] = useState(false);
   const [showDocumentacionModal, setShowDocumentacionModal] = useState(false);
+  const [showCodigoModal, setShowCodigoModal] = useState(false);
+  const [showReconocimientoModal, setShowReconocimientoModal] = useState(false);
   const [estado, setEstado] = useState('');
   const [permiso, setPermiso] = useState('');
   const [motivo, setMotivo] = useState('');
@@ -34,17 +36,38 @@ function SeguimientoPersonal() {
   });
   const [codigo, setCodigo] = useState('');
   const [codigoAsignado, setCodigoAsignado] = useState(false);
-  const [showCodigoModal, setShowCodigoModal] = useState(false);
   const [mensajeGuardado, setMensajeGuardado] = useState('');
-  
+
+  const [tipoReconocimiento, setTipoReconocimiento] = useState('');
+  const [institucion, setInstitucion] = useState('');
+  const [fechaReconocimiento, setFechaReconocimiento] = useState(null);
+  const [anioReconocimiento, setAnioReconocimiento] = useState('');
+  const [tipoMemo, setTipoMemo] = useState('');
+  const [autorizadoPor, setAutorizadoPor] = useState('');
+  const [listaUsuarios, setListaUsuarios] = useState([]);
+  const [grado, setGrado] = useState('');
+
   const navigate = useNavigate();
 
   useEffect(() => {
     const unidadAutenticada = localStorage.getItem('userUnit');
     if (unidadAutenticada) {
       setUserUnit(unidadAutenticada);
+      cargarUsuarios(unidadAutenticada); // Cargar los usuarios de la misma unidad
     }
   }, []);
+
+  const cargarUsuarios = (unidad) => {
+    const db = getDatabase();
+    const usuariosRef = ref(db, 'fundacion/personal');
+    onValue(usuariosRef, (snapshot) => {
+      const usuarios = snapshot.val();
+      const usuariosDeUnidad = usuarios
+        ? Object.values(usuarios).filter((usuario) => usuario.unidad === unidad)
+        : [];
+      setListaUsuarios(usuariosDeUnidad);
+    });
+  };
 
   const handleSignOut = async () => {
     try {
@@ -152,6 +175,38 @@ function SeguimientoPersonal() {
     }
   };
 
+  const handleSaveReconocimiento = () => {
+    const db = getDatabase();
+    if (personalData && personalData.ci) {
+      const nuevoHistorial = {
+        fecha: new Date().toISOString(),
+        tipoReconocimiento,
+        institucion,
+        fechaReconocimiento,
+        anioReconocimiento,
+        tipoMemo: tipoReconocimiento === 'memorandum' ? tipoMemo : null,
+        grado: tipoMemo === 'ascenso' ? grado : null, // Guardar el grado solo si es ascenso
+        motivo,
+        autorizadoPor: tipoReconocimiento === 'memorandum' ? autorizadoPor : null,
+      };
+  
+      update(ref(db, `fundacion/personal/${personalData.ci}`), {
+        historial: [...historial, nuevoHistorial],
+      }).then(() => {
+        setPersonalData((prev) => ({ ...prev, historial: [...historial, nuevoHistorial] }));
+        setShowReconocimientoModal(false);
+        setMensajeGuardado('Reconocimiento guardado exitosamente.');
+        setTimeout(() => {
+          setMensajeGuardado('');
+        }, 2000);
+      }).catch((error) => {
+        console.error("Error updating reconocimiento:", error);
+      });
+    } else {
+      console.error("No se pudo guardar el reconocimiento porque el ID es indefinido.");
+    }
+  };
+
   const toggleHistorial = () => {
     setShowHistorial(!showHistorial);
   };
@@ -212,11 +267,12 @@ function SeguimientoPersonal() {
                 <Button variant="info" className="mr-2 mx-3" onClick={toggleHistorial}>Ver Historial</Button>
                 <Button variant="warning" className="mx-3" onClick={() => setShowDocumentacionModal(true)}>Gestión Documentación</Button>
                 <Button variant="warning" className="mx-3" onClick={() => setShowCodigoModal(true)}>Gestión Código</Button>
+                <Button variant="info" className="mx-3" onClick={() => setShowReconocimientoModal(true)}>Reconocimientos</Button>
               </div>
 
               {showHistorial && (
                 <div className="mt-4">
-                   <h4 className="text-center mb-4">Historial</h4>
+                  <h4 className="text-center mb-4">Historial</h4>
                   {historial.length > 0 ? (
                     <div className="historial-cards">
                       {historial.map((entry, index) => (
@@ -226,16 +282,33 @@ function SeguimientoPersonal() {
                               {format(new Date(entry.fecha), "dd/MM/yyyy HH:mm", { locale: es })}
                             </Card.Title>
                             <Card.Text>
-                              <strong>Estado:</strong> {entry.estado} <br />
-                              {entry.permiso && (
-                                <>
-                                  <strong>Permiso:</strong> {entry.permiso} <br />
-                                  <strong>Fechas del Permiso:</strong> 
-                                  {entry.fechaPermiso ? `${format(new Date(entry.fechaPermiso[0]), "dd/MM/yyyy")} - ${format(new Date(entry.fechaPermiso[1]), "dd/MM/yyyy")}` : 'N/A'} 
-                                  <br />
-                                </>
-                              )}
-                              <strong>Motivo:</strong> {entry.motivo || 'N/A'}
+  <strong>Estado:</strong> {personalData.estado} <br />
+  <strong>Grado:</strong> {entry.grado} <br />
+  {entry.permiso && (
+    <>
+      <strong>Permiso:</strong> {entry.permiso} <br />
+      <strong>Fechas del Permiso:</strong> 
+      {entry.fechaPermiso ? `${format(new Date(entry.fechaPermiso[0]), "dd/MM/yyyy")} - ${format(new Date(entry.fechaPermiso[1]), "dd/MM/yyyy")}` : 'N/A'} 
+      <br />
+    </>
+  )}
+  <strong>Motivo:</strong> {entry.motivo || 'N/A'}<br />
+  {entry.tipoReconocimiento && (
+    <>
+      <strong>Reconocimiento:</strong> {entry.tipoReconocimiento} <br />
+      <strong>Institución:</strong> {entry.institucion || 'N/A'} <br />
+      <strong>Fecha:</strong> {entry.fechaReconocimiento ? format(new Date(entry.fechaReconocimiento), "dd/MM/yyyy") : 'N/A'} <br />
+      <strong>Año:</strong> {entry.anioReconocimiento || 'N/A'} <br />
+      {entry.tipoMemo && (
+        <>
+          <strong>Tipo de Memorandum:</strong> {entry.tipoMemo} <br />
+          <strong>Autorizado por:</strong> 
+          {entry.grado ? `${entry.grado} ` : ''} 
+          {entry.autorizadoPor || 'N/A'}   {personalData.apellidoPaterno || 'N/A'} {personalData.apellidoMaterno || 'N/A'} <br />
+        </>
+      )}
+    </>
+  )}
                             </Card.Text>
                           </Card.Body>
                         </Card>
@@ -282,6 +355,114 @@ function SeguimientoPersonal() {
                       <Button variant="primary" onClick={handleSaveCodigo}>Guardar</Button>
                     </>
                   )}
+                </Modal.Footer>
+              </Modal>
+
+              {/* Modal para Gestión de Reconocimientos */}
+              <Modal show={showReconocimientoModal} onHide={() => setShowReconocimientoModal(false)} centered>
+                <Modal.Header closeButton>
+                  <Modal.Title>Reconocimientos</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                  <Form.Group controlId="formTipoReconocimiento">
+                    <Form.Label>Seleccione:</Form.Label>
+                    <Form.Control as="select" value={tipoReconocimiento} onChange={(e) => setTipoReconocimiento(e.target.value)}>
+                      <option value="">Seleccione...</option>
+                      <option value="reconocimiento">Reconocimiento</option>
+                      <option value="memorandum">Memorandum</option>
+                      <option value="llamada">Llamada de Atención</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                  <Form.Group controlId="formInstitucion" className="mt-3">
+                    <Form.Label>Institución:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={institucion}
+                      onChange={(e) => setInstitucion(e.target.value)}
+                      placeholder="Nombre de la institución"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formFechaReconocimiento" className="mt-3">
+                    <Form.Label>Fecha:</Form.Label>
+                    <DatePicker
+                      selected={fechaReconocimiento}
+                      onChange={(date) => setFechaReconocimiento(date)}
+                      dateFormat="dd/MM/yyyy"
+                      className="form-control"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formAnioReconocimiento" className="mt-3">
+                    <Form.Label>Año:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={anioReconocimiento}
+                      onChange={(e) => setAnioReconocimiento(e.target.value)}
+                      placeholder="Ingrese el año"
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formMotivo" className="mt-3">
+                    <Form.Label>Motivo:</Form.Label>
+                    <Form.Control
+                      type="text"
+                      value={motivo}
+                      onChange={(e) => setMotivo(e.target.value)}
+                      placeholder="Describe el motivo"
+                    />
+                  </Form.Group>
+
+                  {tipoReconocimiento === 'memorandum' && (
+                    <>
+                      <Form.Group controlId="formTipoMemo" className="mt-3">
+                        <Form.Label>Seleccione:</Form.Label>
+                        <Form.Control as="select" value={tipoMemo} onChange={(e) => setTipoMemo(e.target.value)}>
+                          <option value="">Seleccione...</option>
+                          <option value="ascenso">Ascenso</option>
+                          <option value="suspension">Suspensión</option>
+                          <option value="mision">Misión</option>
+                        </Form.Control>
+                      </Form.Group>
+
+                      {tipoMemo === 'ascenso' && (
+                        <Form.Group controlId="formGrado" className="mt-3">
+                          <Form.Label>Grado:</Form.Label>
+                          <Form.Control as="select" value={grado} onChange={(e) => setGrado(e.target.value)}>
+                            <option value="">Seleccione...</option>
+                            <option value="Vol.Inicial">Vol.Inicial</option>
+                            <option value="Vol.2do">Vol.2do</option>
+                            <option value="Vol.1ro">Vol.1ro</option>
+                            <option value="Especialista">Especialista</option>
+                            <option value="Tte.2do">Tte.2do</option>
+                            <option value="Tte.1ro">Tte.1ro</option>
+                            <option value="Capitan">Capitan</option>
+                            <option value="Cap.Director">Cap.Director</option>
+                            <option value="Comodoro">Comodoro</option>
+                            <option value="Comodoro Brigadier">Comodoro Brigadier</option>
+                            <option value="Comodoro Comando">Comodoro Comando</option>
+                          </Form.Control>
+                        </Form.Group>
+                      )}
+
+                      <Form.Group controlId="formAutorizadoPor" className="mt-3">
+                        <Form.Label>Autorizado por:</Form.Label>
+                        <Form.Control as="select" value={autorizadoPor} onChange={(e) => setAutorizadoPor(e.target.value)}>
+                          <option value="">Seleccione...</option>
+                          {listaUsuarios.map((usuario) => (
+                            <option key={usuario.ci} value={usuario.nombre} >
+                              {usuario.nombre} {usuario.apellidoPaterno} {usuario.apellidoMaterno}
+                            </option>
+                          ))}
+                        </Form.Control>
+                      </Form.Group>
+                    </>
+                  )}
+                </Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={() => setShowReconocimientoModal(false)}>Cancelar</Button>
+                  <Button variant="primary" onClick={handleSaveReconocimiento}>Guardar</Button>
                 </Modal.Footer>
               </Modal>
 
