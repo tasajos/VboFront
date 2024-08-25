3174c0bbf27be7a34dbdb919d38e51e331b68fef
tasajos <tasajos@gmail.com>
2024-08-24 22:41:07 -0400

    update

--- vbofront/src/components/Fundacion/Personal/SeguimientoPersonal.js
+++ vbofront/src/components/Fundacion/Personal/SeguimientoPersonal.js
@@ -4,7 +4,7 @@ import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './SeguimientoPersonal.css';
-import NavBar from '../../NavBar/navbar';
+import NavBar from '../../NavBar/navbar';  // Asegúrate de que la ruta sea correcta
 import { signOut } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '../../../firebase';
@@ -49,59 +49,57 @@ function SeguimientoPersonal() {
   };
 
   return (
-    <>
+    <div>
       <NavBar handleSignOut={handleSignOut} />
-      <div className="main-container">
-        <div className="seguimiento-personal-page">
-          <div className="seguimiento-personal-container">
-            <h2 className="text-center mb-4">Seguimiento de Personal</h2>
-            <Form className="d-flex justify-content-center mb-4 search-form" onSubmit={handleSubmit}>
-              <Form.Control
-                type="text"
-                placeholder="Buscar por carnet de identidad"
-                className="mr-2 search-input"
-                value={ci}
-                onChange={(e) => setCi(e.target.value)}
-              />
-              <Button variant="primary" type="submit" className="search-button">
-                Buscar
-              </Button>
-            </Form>
+      <div className="seguimiento-personal-container">
+        <div className="content-wrapper">
+          <h2 className="text-center mb-4">Seguimiento de Personal</h2>
+          <Form className="d-flex justify-content-center mb-4 search-form" onSubmit={handleSubmit}>
+            <Form.Control
+              type="text"
+              placeholder="Buscar por carnet de identidad"
+              className="mr-2 search-input"
+              value={ci}
+              onChange={(e) => setCi(e.target.value)}
+            />
+            <Button variant="primary" type="submit" className="search-button">
+              Buscar
+            </Button>
+          </Form>
 
-            {error && <p className="text-center text-danger error-message">{error}</p>}
+          {error && <p className="text-center text-danger error-message">{error}</p>}
 
-            {personalData && (
-              <Card className="personal-card mx-auto">
-                <Card.Body className="d-flex align-items-center">
-                  <div className="card-left">
-                    <Card.Img
-                      variant="top"
-                      src="https://via.placeholder.com/150" // Aquí podrías agregar la URL de la foto del voluntario si existe
-                      className="profile-image"
-                    />
-                    <div className="name-section">
-                      <h4 className="name-text">{personalData.nombre} {personalData.apellidoPaterno} {personalData.apellidoMaterno}</h4>
-                      <h6 className="role-text">Voluntario</h6>
-                    </div>
+          {personalData && (
+            <Card className="personal-card mx-auto">
+              <Card.Body className="d-flex align-items-center">
+                <div className="card-left">
+                  <Card.Img
+                    variant="top"
+                    src="https://via.placeholder.com/150" // Aquí podrías agregar la URL de la foto del voluntario si existe
+                    className="profile-image"
+                  />
+                  <div className="name-section">
+                    <h4 className="name-text">{personalData.nombre} {personalData.apellidoPaterno} {personalData.apellidoMaterno}</h4>
+                    <h6 className="role-text">Voluntario</h6>
                   </div>
-                  <div className="card-right">
-                    <Card.Text className="card-text">
-                      <strong>CI:</strong> {personalData.ci} <br />
-                      <strong>RH:</strong> {personalData.rh || 'N/A'} <br />
-                      <strong>E-mail:</strong> {personalData.correo} <br />
-                      <strong>Teléfono:</strong> {personalData.telefono} <br />
-                      <strong>Dirección:</strong> {personalData.direccion} <br />
-                      <strong>Ciudad:</strong> {personalData.ciudad} <br />
-                      <strong>Unidad:</strong> {personalData.unidad}
-                    </Card.Text>
-                  </div>
-                </Card.Body>
-              </Card>
-            )}
-          </div>
+                </div>
+                <div className="card-right">
+                  <Card.Text className="card-text">
+                    <strong>CI:</strong> {personalData.ci} <br />
+                    <strong>RH:</strong> {personalData.rh || 'N/A'} <br />
+                    <strong>E-mail:</strong> {personalData.correo} <br />
+                    <strong>Teléfono:</strong> {personalData.telefono} <br />
+                    <strong>Dirección:</strong> {personalData.direccion} <br />
+                    <strong>Ciudad:</strong> {personalData.ciudad} <br />
+                    <strong>Unidad:</strong> {personalData.unidad}
+                  </Card.Text>
+                </div>
+              </Card.Body>
+            </Card>
+          )}
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
